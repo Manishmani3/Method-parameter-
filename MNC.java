2b847cb93dae93d8d5581325b8cac750b63c363f
Manishmani3 <113573478+Manishmani3@users.noreply.github.com>
2022-09-27 12:47:08 +0530

    Add files via upload

---
+++ MNC.java
@@ -0,0 +1,11 @@
+public class MNC {
+public static void main (String []args){
+ITemployee rabin=new ITemployee();
+rabin.name= "Rabin";
+rabin.age=22;
+System.out.println(rabin.name);
+System.out.println(rabin.age);
+System.out.println(rabin.afternoonwork("5 bugs fixed"));
+
+}
+}
